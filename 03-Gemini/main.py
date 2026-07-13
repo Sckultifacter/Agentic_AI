238761fe0a88b26fba6fcd95dd148c93f96f6987
Sckultifacter <deeksha.sudheer26@gmail.com>
2026-07-13 12:11:53 +0530

    added gemini via langchain

---
+++ 03-Gemini/main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    temperature=0
+)
+response = llm.invoke("What are AI agents?")
+print(response.content)
