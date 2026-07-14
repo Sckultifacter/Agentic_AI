13ed2b10f585b132ebe4791ef78e7b29ce294aa5
Sckultifacter <deeksha.sudheer26@gmail.com>
2026-07-14 10:12:42 +0530

    changes in main

--- 03-Gemini/main.py
+++ 03-Gemini/main.py
@@ -1,11 +1,24 @@
+# from dotenv import load_dotenv
+# from langchain_google_genai import ChatGoogleGenerativeAI
+
+# load_dotenv()
+
+# llm = ChatGoogleGenerativeAI(
+#     model="gemini-3.5-flash",
+#     temperature=0
+# )
+# response = llm.invoke("What are AI agents?")
+# print(response.content)
+
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-3.5-flash",
-    temperature=0
+model = init_chat_model(
+    "google_genai:gemini-3.5-flash"
 )
-response = llm.invoke("What are AI agents?")
-print(response.content)
+
+response=model.invoke("What is the captial of India?")
+
+print(response.content[0]['text'])
